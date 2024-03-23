ce70fa27f497cfb2dc8fef5bbdfaf50f8ccc93fe
BabasaiMekala29 <110708314+BabasaiMekala29@users.noreply.github.com>
2024-03-23 18:32:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express();
 
 // middleware
 app.use(cors({origin: ['https://guidethrough-frontend.vercel.app'],methods:["POST","GET","PUT"],credentials:true}));
+//added from stackoverflow
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 app.use(cookieParser());
 
